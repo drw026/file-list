85a217632cb6a645bc0d97c63e825672d20c3024
Andrew Reasoa <andrew@zen7.nl>
2018-12-21 21:36:02 +0100

    refactor and added comments

--- scraper.js
+++ scraper.js
@@ -1,34 +1,57 @@
 const list = require('list-contents');
 
+/**
+ * remove all non 'qunit.html' files
+ * 
+ * @param String file 
+ */
+function filterQunit(file) {
+
+    return file.indexOf('qunit.html') > -1;
+
+}
+
+/**
+ * format the object
+ * 
+ * @param {String} file 
+ */
+function createObject(file) {
+
+    return {
+        name: file.split('/').reverse()[0].split('.')[0],
+        path: file, 
+    }
+
+}
+
+/**
+ * sort array by name
+ * 
+ * @param {String} a 
+ * @param {String} b 
+ */
+function sortByName(a, b) {
+
+    const fileA = a.name.toUpperCase();
+    const fileB = b.name.toUpperCase();
+
+    if (fileA < fileB) return -1; 
+    if (fileA > fileB) return 1;
+    return 0;
+
+}
+
 module.exports = function(req, responds) {
 
+    // get all files and format it before return
     list('./files', function(output) {
 
-        const list = output.files;
+        const data = output.files;
 
-        const createdList = list.filter(function(file) {
-
-            return file.indexOf('qunit.html') > -1;
-
-        }).map(function(file) {
-            
-            return {
-                name: file.split('/').reverse()[0].split('.')[0],
-                path: file, 
-            }
-
-        }).sort(function(a, b) {
-
-            const fileA = a.name.toUpperCase();
-            const fileB = b.name.toUpperCase();
-
-            if (fileA < fileB) return -1; 
-            if (fileA > fileB) return 1;
-            return 0;
-
-        });
-
-        responds.json(createdList);
+        responds.json(
+            data.filter(filterQunit).map(createObject).sort(sortByName)
+        );
 
     });
 
