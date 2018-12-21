718e27e2832825d0a236d3ba3163901171a235a0
Andrew Reasoa <andrew@zen7.nl>
2018-12-21 21:17:37 +0100

    return sorted object

--- scraper.js
+++ scraper.js
@@ -1,16 +1,35 @@
-let fs = require('fs');
-let path = require('path');
-let list = require('list-contents');
-
-
+const list = require('list-contents');
 
 module.exports = function(req, responds) {
 
     list('./files', function(output) {
 
-        responds.send(output.files)
+        const list = output.files;
+
+        const createdList = list.filter(function(file) {
+
+            return file.indexOf('qunit.html') > -1;
+
+        }).map(function(file) {
+            
+            return {
+                name: file.split('/').reverse()[0].split('.')[0],
+                path: file, 
+            }
+
+        }).sort(function(a, b) {
+
+            const fileA = a.name.toUpperCase();
+            const fileB = b.name.toUpperCase();
+
+            if (fileA < fileB) return -1; 
+            if (fileA > fileB) return 1;
+            return 0;
+
+        });
+
+        responds.json(createdList);
 
     });
-
 
 };
